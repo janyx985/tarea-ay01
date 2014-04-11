138b841b2a0c2ac89a95eaec4862cada4b23aec0
janyx985 <janyx985@hotmail.com>
2014-04-11 00:46:48 -0300

    Update javi.php

--- javi.php
+++ javi.php
@@ -19,7 +19,8 @@
 	<section id="contenido">
 		<h1>usando html5 + css</h1>
 		<img src="img/ale.jpg" alt="debe ir la foto de alejandra">
-		<a href="https://github.com/janyx985/tarea2ayudantia.git">código en github</a>
+		<p>alejandra muñoz</p>
+		<a href="http://www.github.com">código en github</a>
 	</section>
 	<footer>
 		<?php
