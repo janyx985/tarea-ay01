971a5261722176a0e1c5502676256361230ad906
janyx985 <janyx985@hotmail.com>
2014-04-11 00:23:58 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>		<!--se carga antes que la página en sí misma-->
+	<meta charset="utf-8"/>
+	<meta name="description" content="uso básico de html5"/>
+	<link rel="shorcut icon" type="image/x-icon" href="img/stock_task.ico">
+	<title>Web básica HTML5</title>
+	<link rel="stylesheet" type="text/css" href="css/principal.css">
+</head>
+<body>
+	<header>
+		<ul>
+			<li><a href="index.php">home</a></li>
+			<li><a href="marcial.php">marcial</a></li>
+			<li><a href="javi.php">alejandra</a></li>
+			<li><a href="henry.php">henry</a></li>
+		</ul>
+	</header>
+	<section id="contenido">
+		<h1>usando html5 + css</h1>
+		<img src="img/logo-utem.jpg" alt="debe ir el logo de la U">
+		<a href="http://www.github.com">código en github</a>
+	</section>
+	<footer>
+		<?php
+		$frases = array(
+		'el fin justifica los medios, nicolas maquiavelo',
+		'el cuerpo es la prisión del alma inmortal, platon',
+		'cada día sabemos más y entendemos menos, albert einstein'
+		);
+
+		// Elegimos un valor entre 0 y el total
+		$i=rand(0,count($frases)-1);
+
+		// Mostramos
+		echo $frases[$i];
+		?> 
+	</footer>
+</body>
+</html>
