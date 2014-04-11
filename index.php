1fcdfe6d859f7fbdc5bf68b4012a49fbc93f5202
janyx985 <janyx985@hotmail.com>
2014-04-11 00:35:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	<section id="contenido">
 		<h1>usando html5 + css</h1>
 		<img src="img/logo-utem.jpg" alt="debe ir el logo de la U">
-		<a href="http://www.github.com">código en github</a>
+		<a href="https://github.com/janyx985/tarea2ayudantia.git">código en github</a>
 	</section>
 	<footer>
 		<?php
